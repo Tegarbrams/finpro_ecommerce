dae872f4317ff12766091d0d85e140af5c4e2b03
unknown <tegar24rd@gmail.com>
2025-06-20 15:15:14 +0700

    tegar

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -141,9 +141,9 @@
                                                 class="focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
                                                 placeholder="Email" aria-label="Email" aria-describedby="email-addon"
                                                 value="{{ old('email') }}" required />
-                                            @error('email')
+                                            {{-- @error('email')
                                                 <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                                            @enderror
+                                            @enderror --}}
                                         </div>
 
 
@@ -153,9 +153,9 @@
                                                 class="focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
                                                 placeholder="Password" aria-label="Password"
                                                 aria-describedby="password-addon" required />
-                                            @error('password')
+                                            {{-- @error('password')
                                                 <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                                            @enderror
+                                            @enderror --}}
 
                                         </div>
 
